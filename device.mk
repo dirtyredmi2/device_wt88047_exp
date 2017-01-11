0d379aebfdd03b153e41e3dfd431c9f3030baab7
Jaekyun Seok <jaekyun@google.com>
2017-01-11 18:53:57 +0900

    Replace ADDITIONAL_DEFAULT_PROPERTIES
    ADDITIONAL_DEFAULT_PROPERTIES should be replaced with
    PRODUCT_DEFAULT_PROPERTY_OVERRIDES.
    
    Test: building succeeded.
    Bug: 34211489
    Change-Id: I57d06cc457573dee6b87959efc276b98e2d795d4

--- device.mk
+++ device.mk
@@ -370,6 +370,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# USB ID
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.usb.vid=2717 \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=ff60 \
+    ro.usb.id.mtp_adb=ff68 \
+    ro.usb.id.ptp=ff10 \
+    ro.usb.id.ptp_adb=ff18 \
+    ro.usb.id.ums=ff20 \
+    ro.usb.id.ums_adb=ff28
+
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
