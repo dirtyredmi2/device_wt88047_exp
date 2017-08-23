a59d176067fd463721a82eb87cc6f70bdb8042f2
Louis Popi <theh2o64@gmail.com>
2017-08-23 22:07:57 +0200

    wt88047: Change vendor headers to msm8x09

--- device.mk
+++ device.mk
@@ -397,7 +397,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
 # MSM8916 Headers
-PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8916/kernel-headers
+PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8x09/kernel-headers
 
 # WiFi HAL
 PRODUCT_PACKAGES += \
