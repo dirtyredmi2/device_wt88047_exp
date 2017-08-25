c35c9d4c36d8fc502c8fb8cf939ca9c4105fe8a4
ssawrav <s.sawrav@gmail.com>
2017-08-25 13:30:42 +0530

    Temporarily Nuke ANT+

--- device.mk
+++ device.mk
@@ -85,16 +85,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=false
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-#Permissions
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
